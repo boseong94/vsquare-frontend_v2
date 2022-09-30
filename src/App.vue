4c901592c7f94bc7ee9bf8a6d391306d2fd855be
boseong94 <boseong.kim@catenoid.net>
2022-09-30 16:33:14 +0900

    pinia, vue router 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+
+  </div>
 </template>
